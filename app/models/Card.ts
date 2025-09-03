b3ffdc76cab636e8f72fd1c295f9746278f2ddec
tanermaddocks <maddockstaner@gmail.com>
2025-09-03 16:26:42 +1000

    feat: Installed typegoose and made card model.

---
+++ app/models/Card.ts
@@ -0,0 +1,37 @@
+import { getModelForClass, modelOptions, prop, Severity } from '@typegoose/typegoose';
+
+enum Status {
+  TODO = 'to-do',
+  DOING = 'doing',
+  DONE = 'done',
+}
+
+@modelOptions({
+  schemaOptions: {
+    timestamps: true,
+  },
+  options: {
+    allowMixed: Severity.ALLOW
+  }
+})
+
+class Card {
+  @prop({
+    required: true,
+    trim: true,
+  })
+  public cardBody!: string;
+
+  @prop({
+    required: true,
+    enum: Status,
+  })
+  public status!: string;
+
+  public dueDate?: Date;
+  public createdAt?: Date;
+  public updatedAt?: Date;
+}
+
+const CardModel = getModelForClass(Card);
+export default CardModel;
