ab03422b2d5e3d7018f38c68b5b5ae05858b4edf
tanermaddocks <maddockstaner@gmail.com>
2025-09-10 10:54:20 +1000

    feat: Users that attempt to got to '/home' without an id added with be redirected to the landing page

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,31 +1,5 @@
+import { redirect } from "next/navigation";
 
-'use client';
-
-import Cards from "@/app/ui/cards";
-import { fetchUserHome } from "../lib/data";
-import { useParams } from "next/navigation";
-
-
-
-export default async function Page() {
-
-  const params = useParams<{ id: string }>();
-  const id = params.id;
-
-  if (!id) {
-    return (
-      <h1>Not logged in</h1>
-    )
-  }
-
-  const { username, cards } = await fetchUserHome({ id });
-
-  return (
-    <div>
-      <main>
-        <h1>Welcome {username}</h1>
-        <Cards cards={cards} />
-      </main>
-    </div>
-  )
+export default function Page() {
+  redirect('/')
 }
